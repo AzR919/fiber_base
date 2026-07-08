31a5079d3995b9f3d6e338157fcc555b49064ff9
AzR919 <azaanrehman919@gmail.com>
2026-07-08 00:01:53 -0700

    temp

--- slurm_batch_command.sh
+++ slurm_batch_command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=2026-07-02_03_atac_base_deep01
+#SBATCH --job-name=2026-07-07_04_h3k4me3_base_deep01_bigger_kernel_w_fire
 #SBATCH --account=def-maxwl
 #SBATCH --output=logs/%x.out
 #SBATCH --error=logs/%x.err
@@ -38,10 +38,10 @@ source /home/azr/lab/misc/menv/bin/activate
 # Run the training
 python main.py \
     --fiber_data_path /home/azr/projects/def-maxwl/azr/data/DATA_FIBER/GM12878/GM12878-fire-v0.1-filtered.cram \
-    --other_data_path /home/azr/projects/def-maxwl/azr/data/DATA_FIBER/GM12878/ENCFF603BJO_ATAC_seq.bigWig \
-    --batch_size 16 --epochs 25 --model fiber_conv_1d --fibers_per_entry 200 --input_flags 1 1 1 1 0 \
-    --res_dir ./results --decoder_type avg --kernel_size 15 \
-    --name_suffix atac_base_deep01
+    --other_data_path /home/azr/projects/def-maxwl/azr/data/DATA_FIBER/GM12878/ENCFF287HAO_H3K4me3.bigWig \
+    --batch_size 16 --epochs 25 --model deep01 --fibers_per_entry 200 --input_flags 1 1 1 1 1 \
+    --res_dir ./results --decoder_type avg --kernel_size 21 \
+    --name_suffix h3k4me3_base_deep01_bigger_kernel_w_fire
 
 # ENCFF798KYP_H3K27ac.bigWig
 # ENCFF287HAO_H3K4me3.bigWig
