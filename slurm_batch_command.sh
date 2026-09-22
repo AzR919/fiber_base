2e563ccf64be385f02490e75465ecf6d3ae550bc
Azaan Rehman <azaanrehman919@gmail.com>
2026-09-22 11:24:29 -0700

    updated runs

--- slurm_batch_command.sh
+++ slurm_batch_command.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=2026-09-20_04_GM12878_K562_A
+#SBATCH --job-name=2026-09-21_01_GM12878_K562_HepG2_A
 #SBATCH --account=def-maxwl_gpu
 #SBATCH --gres=gpu:nvidia_h100_80gb_hbm3_2g.20gb:1
 #SBATCH --output=logs/%x.out
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=16G
-#SBATCH --time=0-5:00:00
+#SBATCH --time=0-6:00:00
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=ara199@sfu.ca
 
@@ -29,11 +29,11 @@ source /home/azr/projects/def-maxwl/azr/misc/menv/bin/activate
 
 # Run the training
 python main.py \
-  --data_config configs/data/data16_GM12878_K562_200U.yaml \
+  --data_config configs/data/data17_GM12878_K562_200U_HepG2_200U.yaml \
   --model_config configs/models/model11_uct_mid_all_A.yaml \
   --train_config configs/training/train01.yaml \
   --eval_configs configs/evals/eval15_HepG2_200U_mini.yaml \
-  --name_prefix All_A_GM_K5_run
+  --name_prefix All_A_GM_K5_Hep_run
 
 # Print job completion time
 echo "Job finished on $(date)"
